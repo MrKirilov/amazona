7e6f4026f68c67cf2cba4557c1b797f12a4ae583
MrKirilov <krasimir13@gmail.com>
2021-03-16 20:51:48 +0000

    Initial Commit

---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { detailsUser, updateUserProfile } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import {
+  USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_RESET,
+} from '../constants/userConstants';
+
+export default function ProfileScreen() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const {
+    success: successUpdate,
+    error: errorUpdate,
+    loading: loadingUpdate,
+  } = userUpdateProfile;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!user) {
+      dispatch({ type: USER_UPDATE_PROFILE_RESET });
+      dispatch(detailsUser(userInfo._id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, userInfo._id, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // dispatch update profile
+    if (password !== confirmPassword) {
+      alert('Passwords Do Not Match!');
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password }));
+    }
+  };
+
+  return (
+    <div>
+      <form className='form' onSubmit={submitHandler}>
+        <div>
+          <h1>User Profile</h1>
+        </div>
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant='danger'>{error}</MessageBox>
+        ) : (
+          <>
+            {loadingUpdate && <LoadingBox />}
+            {errorUpdate && (
+              <MessageBox variant='danger'>{errorUpdate}</MessageBox>
+            )}
+            {successUpdate && (
+              <MessageBox variant='success'>
+                Profile Updated Successfully!
+              </MessageBox>
+            )}
+            <div>
+              <label htmlFor='name'>Name</label>
+              <input
+                type='text'
+                id='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                id='password'
+                placeholder='Enter password'
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor='confirmPassword'>Password</label>
+              <input
+                type='password'
+                id='confirmPassword'
+                placeholder='Confirm password'
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label />
+              <button type='submit' className='primary'>
+                Update Profile
+              </button>
+            </div>
+          </>
+        )}
+      </form>
+    </div>
+  );
+}
