1f1ba065f52cb38feaf1247b0e31f388eff63b8f
MrKirilov <krasimir13@gmail.com>
2021-03-17 10:40:03 +0000

    Added function to check admin status

--- backend/utils.js
+++ backend/utils.js
@@ -43,3 +43,11 @@ export const isAdmin = (req, res, next) => {
     res.status(401).send({ message: 'Invalid Admin Token' });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
